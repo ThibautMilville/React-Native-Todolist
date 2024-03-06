e2d381d32031bf9ed9d1c6b4d70778970866123a
ThibautMilville <tmilville.pro@gmail.com>
2024-03-06 13:57:25 +0100

    Start project + layouts

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { styles } from "./assets/css/app.style";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.app}>
+        <View style={styles.header}>
+          <Text>Header</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Body</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text>Footer</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/css/app.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#F0F0F0",
+  },
+  header: {
+    backgroundColor: "red",
+    flex: 1,
+  },
+  body: {
+    backgroundColor: "blue",
+    flex: 5,
+  },
+  footer: {
+    flex: 0.5,
+  },
+});
